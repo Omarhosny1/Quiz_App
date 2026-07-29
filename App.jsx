e60c727a8d0f7a50005fbb05729ed2f73b4caf5c
Omar Hosny <145453526+Omarhosny1@users.noreply.github.com>
2026-07-29 19:34:11 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,138 @@
+import { useReducer, useEffect } from "react";
+import Header from "./component/Header";
+import Main from "./component/Main";
+import Loader from "./component/Loader";
+import Error from "./component/Error";
+import StartScreen from "./component/StartScreen";
+import Question from "./component/Question";
+import Footer from "./component/Footer";
+import NextButton from "./component/NextButton";
+import Progress from "./component/Progress";
+import FinishScreen from "./component/FinishScreen";
+import Timer from "./component/Timer";
+const SEC_PER_QUESTION = 30;
+const initialState = {
+  questions: [],
+  status: "loading",
+  answer: null,
+  index: 0,
+  points: 0,
+  highScore: 0,
+  secoundRemenin: null,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    case "start":
+      return {
+        ...state,
+        status: "active",
+        secoundRemenin: state.questions.length * SEC_PER_QUESTION,
+      };
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    case "newQuestion":
+      return { ...state, index: state.index + 1, answer: null };
+    case "Finish":
+      return {
+        ...state,
+        status: "Finished",
+        highScore:
+          state.points > state.highScore ? state.points : state.highScore,
+      };
+    case "restart":
+      return { ...initialState, questions: state.questions, status: "ready" };
+    case "tick":
+      return {
+        ...state,
+        secoundRemenin: state.secoundRemenin - 1,
+        status: state.secoundRemenin === 0 ? "Finished" : state.status,
+      };
+    default:
+      throw new Error("Unknown action type");
+  }
+}
+const App = () => {
+  const [
+    { questions, status, answer, index, points, highScore, secoundRemenin },
+    dispatch,
+  ] = useReducer(reducer, initialState);
+  const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0,
+  );
+
+  useEffect(() => {
+    fetch("../data/questions.json")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        dispatch({ type: "dataReceived", payload: data.questions });
+      })
+      .catch(() => {
+        dispatch({ type: "dataFailed" });
+      });
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && (
+          <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              maxPossiblePoints={maxPossiblePoints}
+              points={points}
+              answer={answer}
+            />
+            <Question
+              questions={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <Footer>
+              <Timer dispatch={dispatch} secoundRemenin={secoundRemenin} />
+              <NextButton
+                dispatch={dispatch}
+                answer={answer}
+                numQuestions={numQuestions}
+                index={index}
+              />
+            </Footer>
+          </>
+        )}
+        {status === "Finished" && (
+          <FinishScreen
+            points={points}
+            dispatch={dispatch}
+            highScore={highScore}
+            maxPossiblePoints={maxPossiblePoints}
+          />
+        )}
+      </Main>
+    </div>
+  );
+};
+
+export default App;
